7ed0500bd336ee9047aabf3fd28046cc9cdba92c
cpscott1 <thacam18@gmail.com>
2019-03-16 12:33:29 -0500

    added styling to header

--- instagram/src/components/SearchBar/SearchHeader.js
+++ instagram/src/components/SearchBar/SearchHeader.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import FeatherIcon from 'feather-icons-react';
+import './SearchBar.css';
 import SearchBar from './SearchBar';
 
 const SearchHeader = () => {
    return (
-     <header>
-      <FeatherIcon icon='instagram' />
-      <h1>Instagram</h1>
+     <header className='header-container'>
+      <div className='left'>
+        <div className="left-icon">
+        <FeatherIcon icon='instagram' />
+        </div>
+        <h1>Instagram</h1>
+      </div>
+      <div className="center">
       <SearchBar />
-      <FeatherIcon icon="compass" />
-      <FeatherIcon icon="heart" />
-      <FeatherIcon icon="user" />
-
+      </div>
+      <div className='right'>
+        <div className='right-icons'>
+        <FeatherIcon icon='compass' />
+        </div>
+        <div className='right-icons'>
+        <FeatherIcon icon='heart' />
+        </div>
+        <div className='right-icons'>
+        <FeatherIcon icon='user' />
+        </div>
+      </div>
     </header>
    )
 }
