ba0f20892a18cce08389877ba9873bc62cc98d74
cpscott1 <thacam18@gmail.com>
2019-03-16 11:37:53 -0500

    added all header content

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,1 +1,13 @@
 import React from 'react';
+
+
+const SearchBar = () => {
+  return (
+    <form>
+      <input type="text" placeholder="Search" />
+    </form>
+  )
+}
+
+
+export default SearchBar;
--- instagram/src/components/SearchBar/SearchHeader.js
+++ instagram/src/components/SearchBar/SearchHeader.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import FeatherIcon from 'feather-icons-react';
+import SearchBar from './SearchBar';
 
 const SearchHeader = () => {
    return (
      <header>
       <FeatherIcon icon='instagram' />
+      <h1>Instagram</h1>
+      <SearchBar />
       <FeatherIcon icon="compass" />
       <FeatherIcon icon="heart" />
       <FeatherIcon icon="user" />
